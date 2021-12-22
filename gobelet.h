97cd24cb562eadd60ac92d4d109f8e16a085dc1c
bastien <92434060+Bastien-co@users.noreply.github.com>
2021-12-22 12:48:33 +0100

    Add files via upload

---
+++ gobelet.h
@@ -0,0 +1,21 @@
+/*
+ * gobelet.h
+ *
+ *  Created on: 17 déc. 2021
+ *      Author: basti
+ */
+
+#ifndef GOBELET_H_
+#define GOBELET_H_
+class de: gobelet{
+private :
+	de des;
+public :
+	getValeur();
+
+};
+
+
+
+
+#endif /* GOBELET_H_ */
